0bffb30254dcd6181e649d219130ecd7f2d11170
Sakil Ahmed <sakilcoder@gmail.com>
2023-04-17 02:15:36 +0600

    ext

--- assets/js/index-ext.js
+++ assets/js/index-ext.js
@@ -1,15 +1,16 @@
 const draw = new MapboxDraw({
+    
     displayControlsDefault: false,
-    // Select which mapbox-gl-draw control buttons to add to the map.
+    
     controls: {
         polygon: true,
-        trash: true
+        trash: true,
+        
     },
-    // Set mapbox-gl-draw to draw by default.
-    // The user does not have to click the polygon control button first.
+   
     defaultMode: 'draw_polygon'
 });
-map.addControl(draw);
+map.addControl(draw, 'top-left');
 
 map.on('draw.create', updateArea);
 map.on('draw.delete', updateArea);
